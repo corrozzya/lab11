0ff4ba7161c4e89f8a3fbca4e237c452931d937c
Alexandra <152961458+corrozzya@users.noreply.github.com>
2025-05-10 14:04:31 +0700

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Главная</router-link> |
+      <router-link to="/products">Товары</router-link> |
+      <router-link to="/about">О нас</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<style>
+nav {
+  padding: 1rem;
+  background-color: #f8f8f8;
+}
+nav a {
+  margin: 0 10px;
+}
+</style>
